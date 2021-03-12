f77ce16bd40a0e9e6cf1bcafb033300334b38266
Anderson <anderws10@gmail.com>
2021-03-12 10:40:53 -0300

    config api gateway

--- hr-payroll/src/main/java/com/estudo/payroll/controller/PaymentController.java
+++ hr-payroll/src/main/java/com/estudo/payroll/controller/PaymentController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.estudo.payroll.entities.Payment;
 import com.estudo.payroll.services.PaymentService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 @RequestMapping("/payments")
@@ -18,8 +19,16 @@ public class PaymentController {
 	PaymentService service;
 		
 	@GetMapping("/{workerId}/days/{days}")
+	 @HystrixCommand(fallbackMethod = "getPaymentFallback")
 	public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days){
 		Payment payment = service.getPayment(workerId, days);
 		return ResponseEntity.ok(payment);
 	}
+	
+	public ResponseEntity<Payment> getPaymentFallback(Long workerId, Integer days){
+		
+			Payment payment = new Payment("Brann", 400.0, days);
+			return ResponseEntity.ok(payment);
+	}
+
 }
--- hr-payroll/src/main/java/com/estudo/payroll/feignclients/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/estudo/payroll/feignclients/WorkerFeignClient.java
@@ -1,5 +1,6 @@
 package com.estudo.payroll.feignclients;
 
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
@@ -10,6 +11,7 @@ import com.estudo.payroll.entities.Worker;
 
 @Component
 @FeignClient(name = "hr-worker", path="workers")
+@RibbonClient(name = "hr-worker")
 public interface WorkerFeignClient {
 
 	@GetMapping("/{id}")
