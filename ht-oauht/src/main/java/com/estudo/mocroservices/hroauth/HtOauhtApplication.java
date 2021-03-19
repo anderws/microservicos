404edb699eef3eabad6371c57d5ff822ab04acf1
Anderson <anderws10@gmail.com>
2021-03-19 07:58:46 -0300

    Project hr-oauth created

---
+++ ht-oauht/src/main/java/com/estudo/mocroservices/hroauth/HtOauhtApplication.java
@@ -0,0 +1,13 @@
+package com.estudo.mocroservices.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtOauhtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtOauhtApplication.class, args);
+	}
+
+}
